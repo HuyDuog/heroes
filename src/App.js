2b9e310bb8df5f1c54c0977d685a03893474088b
HuyDuog <linhhee2k@gmail.com>
2021-11-02 14:25:57 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Heroes from './heroes/Heroes.js';
+function App() {
+  return (
+    <div className="App">
+      <Heroes></Heroes>
+    </div>
+  );
+}
+
+export default App;
